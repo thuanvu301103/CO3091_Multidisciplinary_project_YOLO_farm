e6973e2b077c060ab6a93204513a31c240d8ec85
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2024-04-03 11:15:14 +0700

    Update envsense.controller.ts

--- yolo-farm/yolo-farmbe/src/envsense/envsense.controller.ts
+++ yolo-farm/yolo-farmbe/src/envsense/envsense.controller.ts
@@ -6,26 +6,26 @@ export class EnvsenseController {
 
     constructor(private readonly envsenseService: EnvsenseService) { }
 
-	@Get()
-	async streamEvent(@Res() res) {
+    @Get()
+    async streamEvent(@Res() res) {
 	// Set up HTTP header for stream Event
 	res.setHeader('Content-Type', 'text/event-stream');
     	res.setHeader('Cache-Control', 'no-cache');
     	res.flushHeaders();
 	
-		// Sent message when changes occurred	
-		this.envsenseService.mqtt_client.on('message', async (topic, message) => {
+	// Sent message when changes occurred	
+	this.envsenseService.mqtt_client.on('message', async (topic, message) => {
 
-            let detail = await this.envsenseService.evaluateVal(topic, message);
-            console.log(detail);
+        let detail = await this.envsenseService.evaluateVal(topic, message);
+        console.log(detail);
 
-            console.log(`Received message on topic ${topic} - feed ${detail.feed_name} - evaluation ${detail.evaluate}: ${message.toString()}`);
-            // Handle the message as needed
-            let res_data = JSON.stringify(await this.envsenseService.updatePlantAreaChage(topic, detail.feed_name, message - 0, detail.evaluate));
-			console.log('Sent data: ', res_data);
-            res.write("data:" + res_data + "\n\n");
+        console.log(`Received message on topic ${topic} - feed ${detail.feed_name} - evaluation ${detail.evaluate}: ${message.toString()}`);
+        // Handle the message as needed
+        let res_data = JSON.stringify(await this.envsenseService.updatePlantAreaChage(topic, detail.feed_name, message - 0, detail.evaluate));
+		console.log('Sent data: ', res_data);
+            	res.write("data:" + res_data + "\n\n");
         });
-	}
+    }
     
     @Get('user/:id/plantarea/list')
     async getListPlantArea(@Param('id') id: string): Promise<any[]> {
